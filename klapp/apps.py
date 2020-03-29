27781cf9ebee62ca6b9928576861d72e6ca155c9
Anandu <letter2anandu@gmail.com>
2020-03-29 19:02:17 +0530

    First commit

---
+++ klapp/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class Klapp2Config(AppConfig):
+    name = 'klapp2'
---
+++ klapp/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+import pytz
+import time
+import netifaces as ni
+
+ni.ifaddresses('wlp6s0')
+ip = ni.ifaddresses('wlp6s0')[ni.AF_INET][0]['addr']
+
+
+# Create your views here.
+
+def index(request):
+    return render(request,'klapp/index.html',{'date_now': time.ctime()}) #third argument give as json this is for dynamic web page/fetch from db etc
